8414e6c24133e1baab545efca596edb54a8a7ce9
unknown <tadej.rola@gmail.com>
2018-12-18 18:15:41 +0200

    day 1

---
+++ advent-of-code-2018/advent-of-code-2018/Program.cs
@@ -0,0 +1,17 @@
+﻿using advent_of_code_2018.src.day1;
+using System;
+
+namespace advent_of_code_2018
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Day1 day1 = new Day1();
+            var frequency = day1.getFrequency();
+            Console.WriteLine("RESULT DAY 1:");
+            Console.WriteLine(frequency);
+            Console.ReadLine();
+        }
+    }
+}
